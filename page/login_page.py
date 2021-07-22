3787361eb3fb06b03e2b3c4784fdd249b3731b0d
吴登科 <513411425@qq.com>
2021-07-22 16:34:55 +0800

    新增csv文件保存登录员工信息

--- page/login_page.py
+++ page/login_page.py
@@ -5,6 +5,7 @@ class LoginPage(Page):
     input_staff_name = Element(name='staff_name', describe="登录用户名")
     input_password = Element(name='password', describe="登录密码")
     login_button = Element(css=".card-body>form>div.form-group>button.btn", describe="登录按钮")
+
     user_error = Element(xpath="//div[@class='card-body']/form/div[1]/div[@class='invalid-feedback']", describe="用户名提示")
     pwd_error = Element(xpath="//div[@class='card-body']/form/div[2]/div[@class='invalid-feedback']", describe="密码提示")
     all_error = Element(xpath="//div[@class='card-body']/form/div[3]/span", describe="全部错误提示")
--- test_dir/test_popup.py
+++ test_dir/test_popup.py
@@ -7,6 +7,8 @@ import sys
 import json
 from time import sleep
 import pytest
+import csv
+import codecs
 from os.path import dirname, abspath
 
 base_path = dirname(dirname(abspath(__file__)))
@@ -18,12 +20,25 @@ from dbConn import DB
 class TestPopup:
     """弹窗相关功能测试"""
 
+    # 读取测试用登录员工数据
+    def get_staff_info(self):
+        csv_dict = {}
+        with codecs.open(base_path + "/test_dir/data/test_staff_info.csv", 'r', encoding='gbk') as fp:
+            fp_key = csv.reader(fp)
+            for csv_key in fp_key:
+                csv_reader = csv.DictReader(fp, fieldnames=csv_key)
+                for row in csv_reader:
+                    csv_dict = dict(row)
+        return csv_dict
+
+    # # 登录公共方法
     def login(self, browser, base_url):
         self.page = PopupPage(browser)
         self.page.get(base_url + '/index.php/employform/index')
         if self.page.get_url == base_url + '/index.php/employform/login/index':
-            self.page.input_staff_name = '唐静'
-            self.page.input_password = 'sh987654'
+            self.staff_info = self.get_staff_info()
+            self.page.input_staff_name = self.staff_info['name']
+            self.page.input_password = self.staff_info['password']
             self.page.login_button.click()
             sleep(15)
 
@@ -62,8 +77,9 @@ class TestPopup:
         3.验证待回访数据
         """
         self.login(browser, base_url)
-        total = self.get_wait_visit_total(1, 5)
+        total = self.get_wait_visit_total(self.staff_info['roles_str'], self.staff_info['staff_id'])
         print(total)
+
         assert 1 > 0
 
     def get_wait_visit_total(self, session_role_id, session_staff_id):
@@ -71,13 +87,22 @@ class TestPopup:
         db_conn = DB(ip='47.103.83.160', user='root', passwd='c587024e9ec3ea0a', db='ylg')
         sql = "select v.visit_id,v.client_id,CAST(v.visit_time AS CHAR) as visit_time,CAST(v.next_visit_time AS CHAR) as next_visit_time from xy_visit v"
         sql += " inner join xy_client c on c.client_id=v.client_id"
-        sql += " where v.staff_id=" + str(session_staff_id) + " and (v.staff_id=c.staff_id or v.staff_id=c.valid_allot_staff)"
+        sql += " where v.staff_id=" + str(
+            session_staff_id) + " and (v.staff_id=c.staff_id or v.staff_id=c.valid_allot_staff)"
         sql += " and v.type='report' and v.wait_visit_status=1"
         if session_role_id in [87, 91]:
             sql += " and c.high_quality = 0"
             sql += " and c.client_rank <> 'D'"
         sql += " order by v.client_id desc,v.visit_time desc,v.next_visit_time desc"
         visits = db_conn.query(sql)
+
+        # 获取客户并加上最新的访问时间和下次访问时间
+        clients=[]
+        if visits:
+            for visit in visits:
+                
+
+
         return visits
 
         # return res
